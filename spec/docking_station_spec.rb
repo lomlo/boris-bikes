8f974becd76b19bc29127b543cb01630598ed07f
Luke <lukeaveil@yahoo.com>
2016-05-24 16:31:21 +0100

    tests for docking station creates working bike, update release bike method and working? method to pass test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,11 @@ describe DockingStation do
 		expect(subject).to respond_to(:release_bike)
 	end
 
+	it 'method release_bike gets working bike' do
+		expect(subject.release_bike).to be_a (Bike)
+	end
+
+	it 'released bike works' do
+		expect(Bike.new).to be_working
+	end
 end 
